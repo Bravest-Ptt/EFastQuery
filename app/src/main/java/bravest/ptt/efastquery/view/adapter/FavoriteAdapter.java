e2bf46986dd7c4b1a414e24231428827610bfde4
root <root@thundersoft.(none)>
2017-02-22 18:22:24 +0800

    Fill Favorite Adapter and holder

--- app/src/main/java/bravest/ptt/efastquery/view/adapter/FavoriteAdapter.java
+++ app/src/main/java/bravest/ptt/efastquery/view/adapter/FavoriteAdapter.java
@@ -2,17 +2,22 @@ package bravest.ptt.efastquery.view.adapter;
 
 import android.view.ViewGroup;
 
+import bravest.ptt.efastquery.R;
 import bravest.ptt.efastquery.view.holder.AbsHolder;
+import bravest.ptt.efastquery.view.holder.FavoriteHolder;
 
 
 public class FavoriteAdapter extends AbsAdapter {
 
     @Override
     public AbsHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        FavoriteHolder holder = new FavoriteHolder(getInflater().inflate(R.layout.item_favorite, null));
+        holder.setItemClickListener(getItemClickListener());
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(AbsHolder absHolder, int position) {
+        
     }
 }
